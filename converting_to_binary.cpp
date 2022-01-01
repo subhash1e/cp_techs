3f99bea56d5f360514b9b8a49c8142deb3edaf0e
Subhash Kumar <85139394+subhash1e@users.noreply.github.com>
2022-01-02 03:58:24 +0530

    Update converting_to_binary.cpp

--- converting_to_binary.cpp
+++ converting_to_binary.cpp
@@ -8,9 +8,15 @@ int main(){
   int x = 10;
   string binn = bitset<8>(x).to_string();
   cout << binn << endl; // prints 00001010
+  
+  
+  
+  //some other conversions 
+  
+  //to octal
   cout << setbase(8) << x << endl; // prints 12 because 1*(8^1) + 2*(8^0) =  12 (base 8 value of 10 )
   // setbase works only for 8 10 and 16 for rest it goes to default value 10
-  
+  // to decimal
   cout << setbase(10) << 011  << endl; // prints 9 because putting 0 before int makes it octal and 011(8) = 9(10)
   cout << setbase(10) << 0x11 << endl; // prints 17 because 11(16) = 17(10)
   cout << setbase(10) << 0b11 << endl; // prints 3 because 11(2) = 3(10)
